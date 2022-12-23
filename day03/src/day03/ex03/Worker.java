35ef6878794167ab675eb0b1684e288fadb13654
daviswillard <kmlzntlln1@gmail.com>
2022-12-23 15:25:09 +0300

    day03 working

--- day03/src/day03/ex03/Worker.java
+++ day03/src/day03/ex03/Worker.java
@@ -7,6 +7,8 @@ import java.net.URL;
 
 public class Worker implements Runnable {
 
+	private final String homeDir = System.getProperty("user.home");
+
 	private final DataObject data;
 
 	public Worker(DataObject data) {
@@ -17,7 +19,8 @@ public class Worker implements Runnable {
 		String current;
 		while (!(current = data.getLink()).isEmpty()) {
 			try (BufferedInputStream stream = new BufferedInputStream(new URL(current).openStream());
-					FileOutputStream outputStream = new FileOutputStream(current)) {
+					FileOutputStream outputStream =
+							new FileOutputStream(homeDir + "/" + current.substring(current.lastIndexOf('/')))) {
 				byte[] buffer = new byte[1024];
 				int bytesRead;
 				while ((bytesRead = stream.read(buffer, 0, 1024)) != -1) {
