f4367dd765211234545d61e7cd02a1996f4ec25d
dwillard <kmlzntlln1@gmail.com>
2023-01-08 13:38:49 +0300

    day07 shame on me

--- day07/Annotation/src/main/java/student/dwillard/annotations/HtmlForm.java
+++ day07/Annotation/src/main/java/student/dwillard/annotations/HtmlForm.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.SOURCE)
 public @interface HtmlForm {
   String fileName() default "user_form.html";
   String action();
